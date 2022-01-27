53b1ac7e9de28119dd207dbc81f8482fe8d770b1
Eddie Yan <Loser777@gmail.com>
2022-01-26 19:00:44 -0800

    switch to pandas

--- data.py
+++ data.py
@@ -2,7 +2,8 @@ import csv
 import os
 
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+# from sklearn.preprocessing import OneHotEncoder
 
 import constants
 
@@ -63,17 +64,18 @@ class StreamSessionData(object):
                 winstreaks[race_to_idx[oppo_race]] += 1
                 lossstreaks[race_to_idx[oppo_race]] = 0
                 lossstreaks[0] = 0
-                outcome = 1
+                rawdata[constants.RESULT_IDX] = 'victory'
+                # outcome = 1
             else:
                 losses[0] += 1
                 losses[race_to_idx[oppo_race]] += 1
                 winstreaks[race_to_idx[oppo_race]] = 0
                 winstreaks[0] = 0
-                outcome = 0
+                # outcome = 0
             rawdata = list(rawdata)
-            rawdata[constants.ARTOSIS_NAME_IDX] = None
-            rawdata[constants.OPPONENT_NAME_IDX] = None
-            self.data.append(rawdata[:constants.RESULT_IDX] + wins + losses + sub(wins, losses) + winstreaks + lossstreaks + [outcome])
+            #rawdata[constants.ARTOSIS_NAME_IDX] = None
+            #rawdata[constants.OPPONENT_NAME_IDX] = None
+            self.data.append(rawdata + wins + losses + sub(wins, losses) + winstreaks + lossstreaks)
 
 class Dataset(object):
     def __init__(self, stream_session_data_list):
@@ -81,20 +83,37 @@ class Dataset(object):
         self.to_one_hot()
 
     def to_one_hot(self):
-        numerical_data = list()
-        categorical_data = list()
-        result = list()
+        #numerical_data = list()
+        #categorical_data = list()
+        #result = list()
+        #for stream_session_data in self.stream_session_data_list:
+        #    for row in stream_session_data.data:
+        #        numerical, categorical = splitnumerical(row[:-1])
+        #        numerical_data.append(numerical)
+        #        categorical_data.append(categorical)
+        #        result.append(row[-1])
+        #self.encoder = OneHotEncoder(drop='first')
+        #self.encoder.fit(categorical_data)
+        #self.categorical_encoded = self.encoder.transform(categorical_data).toarray()
+        #self.data = np.concatenate((np.array(numerical_data), self.categorical_encoded), axis=1)
+        #self.labels = np.array(result)
+        columns = len(self.stream_session_data_list[0].data[0])
+        data_lists = [list() for _ in range(columns)]
         for stream_session_data in self.stream_session_data_list:
             for row in stream_session_data.data:
-                numerical, categorical = splitnumerical(row[:-1])
-                numerical_data.append(numerical)
-                categorical_data.append(categorical)
-                result.append(row[-1])
-        self.encoder = OneHotEncoder(drop='first')
-        self.encoder.fit(categorical_data)
-        self.categorical_encoded = self.encoder.transform(categorical_data).toarray()
-        self.data = np.concatenate((np.array(numerical_data), self.categorical_encoded), axis=1)
-        self.labels = np.array(result)
+                print(row)
+                assert len(row) == columns
+                for idx, value in enumerate(row):
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        pass     
+                    data_lists[idx].append(value)
+        column_labels = constants.SCHEMA + [f'feature{i}' for i in range(0, columns - constants.SCHEMA_LEN)]
+        data_dict = {label:data_lists[i] for i,label in enumerate(column_labels)}
+        self.dataframe = pd.DataFrame(data_dict)
+        self.labels = pd.get_dummies(self.dataframe['outcome'], drop_first=True)
+        self.one_hot = pd.get_dummies(self.dataframe.drop(labels=['player_a_id', 'player_b_id'], axis=1), drop_first=True)
 
 def test():
     data = StreamSessionData('data/2021-12-23.csv')
@@ -106,7 +125,9 @@ def test():
                 data = StreamSessionData(os.path.join(dirpath, filename))
                 rawdataset.append(data)
     dataset = Dataset(rawdataset)
-    print(dataset.data)
+    #print(dataset.data)
+    #print(dataset.labels)
+    print(dataset.one_hot)
     print(dataset.labels)
     print("OK")
             
