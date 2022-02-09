70dfd1e7ba09f75cf250075ea329508744195ec4
Eddie Yan <Loser777@gmail.com>
2022-02-08 20:33:21 -0800

    update train trials

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 csv2model.py -o test -t 5000 --time-validation --additional-drop player_a_rank,player_a_mmr --no-session-features
-python3 csv2model.py -o test -t 5000 --cross-validation --additional-drop player_a_rank,player_a_mmr --no-session-features
 python3 csv2model.py -o test -t 5000 --time-validation --additional-drop player_a_rank,player_a_mmr
-python3 csv2model.py -o test -t 5000 --cross-validation --additional-drop player_a_rank,player_a_mmr
+python3 csv2model.py -o test -t 2000 --cross-validation --additional-drop player_a_rank,player_a_mmr --no-session-features
+python3 csv2model.py -o test -t 2000 --cross-validation --additional-drop player_a_rank,player_a_mmr
