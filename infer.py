51262c7e61bf6b2df1a3acce10045cb9f51e2275
Eddie Yan <Loser777@gmail.com>
2022-02-13 16:17:18 -0800

    fix infer

--- infer.py
+++ infer.py
@@ -10,28 +10,28 @@ import train
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--artommr', type=float, help='artosis mmr', default=0)
+    ## parser.add_argument('--artommr', type=float, help='artosis mmr', default=0)
     parser.add_argument('--oppommr', type=float, help='opponent mmr', default=0)
-    parser.add_argument('--artorace', type=str, help='artosis race', default='t')
+    # parser.add_argument('--artorace', type=str, help='artosis race', default='t')
     parser.add_argument('--opporace', type=str, help='opponent race', required=True)
-    parser.add_argument('--artorank', type=str, help='artosis rank', default='')
+    # parser.add_argument('--artorank', type=str, help='artosis rank', default='')
     parser.add_argument('--opporank', type=str, help='opponent rank', default='')
     # parser.add_argument('--opponame', type=str, help='opponent name', default='')
     parser.add_argument('--history', type=str, help="match history e.g., zwpltw", default='')
     parser.add_argument('--map', type=str, required=True)
     parser.add_argument('--turnrate', type=float, default=0)
     parser.add_argument('--latency', type=str, default='')
-    parser.add_argument('--uptime', type=float, required=True)
+    # parser.add_argument('--uptime', type=float, required=True)
     
     args = parser.parse_args()
-    if args.artommr > 1800:
-        assert len(args.artorank)
+    #if args.artommr > 1800:
+    #    assert len(args.artorank)
     if args.oppommr > 1800:
         assert len(args.opporank)
-    assert args.artorace in 'ptzr', "unknown artosis race"
+    #assert args.artorace in 'ptzr', "unknown artosis race"
     assert args.opporace in 'ptzr', "unknown opponent race"
-    if args.artorace != 't':
-        print("WARNING: artosis race not terran, are you sure?")
+    #if args.artorace != 't':
+    #    print("WARNING: artosis race not terran, are you sure?")
     assert args.artommr < 3000
     assert args.oppommr < 3000
    
@@ -42,9 +42,9 @@ def main():
     assert args.map in map_values, "unknown map"
 
     inp = [time.time(),
-            'artosis'   , args.artorank, args.artommr, args.artorace,
+            'artosis', None, None, 't',
             'opponentnameplaceholder', args.opporank, args.oppommr, args.opporace,
-            args.map, args.turnrate, args.latency, args.uptime, 'defeat']
+            args.map, args.turnrate, args.latency, 0, 'defeat']
     assert len(args.history) % 2 == 0
     modelandparams = list()
     for dirpath, _, filenames in os.walk('.'):
